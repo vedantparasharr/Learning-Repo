33ff937435c9be4a9ca0464151e87b71dcdac8bf
vedantparasharr <iemvedant@gmail.com>
2025-11-29 19:29:51 +0530

    Create local modules using exports and try using that

--- Backend/Node JS/server.js
+++ Backend/Node JS/server.js
@@ -1,15 +1,23 @@
 const os = require('os')
 const path = require('path')
 
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
 
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
 
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename))
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
 
-console.log(path.parse(__filename))
+// console.log(path.parse(__filename))
+
+
+const {add, subtract, multiply, divide} = require('./math');
+
+console.log(add(4,5));
+console.log(divide(4,5));
+console.log(multiply(4,5));
+console.log(subtract(4,5));
