ca5b23d6797b104de864a14b4cd565a75026128c
vedantparasharr <iemvedant@gmail.com>
2025-12-04 22:34:31 +0530

    Introduction to Express JS

--- Backend/Node JS/Express JS/server.js
+++ Backend/Node JS/Express JS/server.js
@@ -1,8 +1,45 @@
 const path = require("path");
-const express = require('express')
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 
+app.get(/^\/$|index(.html)?/, (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+app.get(/^\/new-page(.html)?$/, (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+app.get(/^\/old-page(.html)?$/, (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
 
+app.get(
+  "/hello.html",
+  (req, res, next) => {
+    console.log("attempted to load hello");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World");
+  }
+);
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const one = (req, res, next) => {
+  console.log("One");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("Two");
+  next();
+};
+const three = (req, res) => {
+  console.log("Three");
+};
+
+app.get(/\/test(.html)?/, [one, two, three]);
+
+app.get(/.*/, (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
