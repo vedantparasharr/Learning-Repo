31616ce211d93214cf5791490790109258d6aff1
vedantparasharr <iemvedant@gmail.com>
2025-12-04 14:30:04 +0530

    Initialise Express JS to learn

---
+++ Backend/Node JS/Express JS/server.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
