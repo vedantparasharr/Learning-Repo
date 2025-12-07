41b75a26234778360aa4aef149cfa67564ad09b0
vedantparasharr <iemvedant@gmail.com>
2025-12-07 22:28:42 +0530

    Install Express JS and create a basic server using that.

---
+++ Backend/Express JS/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is about");
+});
+
+app.listen(3000, () => {
+    console.log('Server is running at port 3000')
+});
