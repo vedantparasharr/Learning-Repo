7c33973b540e1c5a51fd501d94f13b0fe8055a3c
vedantparasharr <iemvedant@gmail.com>
2025-12-08 00:23:36 +0530

    Learn middlewares, error handling.

--- Backend/Express JS/server.js
+++ Backend/Express JS/server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
 
+const m1 = (req, res, next) => {
+  console.log("1");
+  next();
+};
+const m2 = (req, res, next) => {
+  console.log("1");
+  next();
+};
+const m3 = (req, res, next) => {
+  console.log("1");
+  next();
+};
+const m4 = (req, res, next) => {
+  console.log("1");
+  next();
+};
+
+app.use([m1, m2, m3, m4]);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -9,6 +28,15 @@ app.get("/about", (req, res) => {
   res.send("This is about");
 });
 
+app.get("/error", (req, res, next) => {
+  return next(new Error("Not implemented"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 app.listen(3000, () => {
-    console.log('Server is running at port 3000')
+  console.log("Server is running at port 3000");
 });
