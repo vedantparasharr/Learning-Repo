3fba6007b3114ed09f7d53e7103ec29e65205f68
vedantparasharr <iemvedant@gmail.com>
2025-12-07 18:58:00 +0530

    Separate the code

--- Backend/script.js
+++ Backend/script.js
@@ -24,3 +24,50 @@ const obj = {
 };
 
 console.log(obj.age);
+
+// fs.writeFile("message.txt", "I am Vedant", (error) => {
+//   if (error) {
+//     console.log(error);
+//   }
+// });
+
+// fs.appendFile("message.txt", "\nI am pursuing computer science.", (error) => {
+//   if (error) {
+//     console.log(error);
+//   }
+// });
+
+// fs.copyFile("message.txt", "destination.txt", (error) => {
+//   if (error) {
+//     console.log(error);
+//   }
+// });
+
+// fs.rename("message.txt", "newMessage.txt", (error) => {
+//   if (error) {
+//     console.log(error);
+//   }
+// });
+
+// fs.unlink("destination.txt", (error) => {
+//   console.log(error);
+// });
+
+// fs.rm("./test", { recursive: true }, (error) => {
+//   if (error) {
+//     console.log(err);
+//   }
+// });
+
+// fs.readFile("newMessage.txt", { encoding: "utf-8" }, (error, data) => {
+//   if (error) console.log(error);
+//   else console.log(data);
+// });
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/about") {
+//     res.end("Hello");
+//   }
+// });
+
+// server.listen(3000);
