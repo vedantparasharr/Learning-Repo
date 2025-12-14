ccf1e6c4205a0d4ede5e4e07a6d204e17f65ec47
vedantparasharr <iemvedant@gmail.com>
2025-12-14 14:14:28 +0530

    Learn sending and storring cookie

--- Backend/Authentication and Authorisation/app.js
+++ Backend/Authentication and Authorisation/app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.cookie("user", "Vedant");
+  res.send("Cookie has been set");
+});
+
+app.get("/read", (req, res) => {
+  console.log(req.cookies);
+  res.send('Reading')
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
